afa3f896304587b9ea8a5852de520948bd990747
cool-pants <muban.sg@gmail.com>
2024-05-23 23:22:45 +0530

    feat: use gocui as tui lib

--- main.go
+++ main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
+	"log"
+
+	"github.com/jroimartin/gocui"
 )
 
 func main() {
-	// disable input buffering
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	// do not display entered characters on the screen
-	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-	fmt.Print("\033[H\033[2J")
-	var b []byte = make([]byte, 1)
-	for {
-		os.Stdin.Read(b)
-		fmt.Println("I got the byte", b, "("+string(b)+")")
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
 	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	// Give botton-left padding of 2 from the edge
+	if v, err := g.SetView("hello", 0, 0, maxX-2, maxY-5); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	if tbline, err := g.SetView("tabline", 0, maxY-4, maxX-2, maxY-2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(tbline, "NORMAL")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
